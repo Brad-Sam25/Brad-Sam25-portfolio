8ca8ff7d5957f0bb1cddd68ca46dae7e3ab8cdec
Brad Samuel <bradsamuel97@gmail.com>
2023-03-02 18:30:52 -0500

    Add content to Home Page

--- Brad-Sam-portfolio/src/pages/Home/Home.jsx
+++ Brad-Sam-portfolio/src/pages/Home/Home.jsx
@@ -1,27 +1,69 @@
 import React from 'react';
-import {Container, Grid, GridItem, Heading, Text} from '@chakra-ui/react';
+import { Container, Grid, GridItem, Heading, Text, Box } from '@chakra-ui/react';
 
 const Home = () => {
-  return (
-    <Container as='section' maxW='4xl'>
-      <Text pb='10px'>Hi, my name is </Text>
-      <Heading as='h1' pb='10px' size='4xl'>Brad Samuel.</Heading>
-      <Text pb='10px'>I love making things on the web</Text>
-      <Text pb='10px'>As a professional web developer, I possess a passion for crafting innovative and intuitive web experiences that effectively contribute to the overall improvement of patient health outcomes while simultaneously driving future funding success. I am committed to delivering exceptional quality and ensuring that the end product not only meets but surpasses the expectations of all stakeholders involved.</Text>
+	return (
+		<Container as="section" maxW="4xl">
+			<Container as='div' p="100px 0px">
+				<Text pb="10px" >Hi, my name is </Text>
+				<Heading as="h1" pb="10px" size="4xl">
+					Brad Samuel.
+				</Heading>
+				<Text pb="10px">I love making things on the web</Text>
+				<Text pb="10px">
+					As a professional web developer, I possess a passion for crafting
+					innovative and intuitive web experiences that effectively contribute to
+					the overall improvement of patient health outcomes while simultaneously
+					driving future funding success. I am committed to delivering exceptional
+					quality and ensuring that the end product not only meets but surpasses
+					the expectations of all stakeholders involved.
+				</Text>
+			</Container>
 
-      <Grid
-        templateRows='repeat(2, 1fr)'
-        templateColumns='repeat(2, 1fr)'
-        gap={4}
-      >
-        <GridItem w='100%' h='10' bg='blue.500'>
-        </GridItem>
-        <GridItem w='100%' h='10' bg='blue.500' />
-        <GridItem w='100%' h='10' bg='blue.500' />
-        <GridItem w='100%' h='10' bg='blue.500' />
-      </Grid>
-    </Container>
-  )
-}
+			<Container as='div' p="100px 0px">
+				<Grid
+					templateAreas={`"title title"
+					"content image"`}
+					gridTemplateRows={'50px 1fr 30px'}
+					gridTemplateColumns={'350px 1fr'}
+					h="200px"
+					gap="1"
+					color="blackAlpha.700"
+					fontWeight="bold"
+				>
+					<GridItem pl="2" area={'title'}>
+						<Heading as="h2" pb="10px" size="xl">
+							About Me
+						</Heading>
+					</GridItem>
+					<GridItem pl="2" area={'content'}>
+						<Text pb="10px">
+							Prior to my career change into the tech sphere &#40;web and software development&#41;, I worked in law firm as a legal assistant with aspirations of eventually becoming a real estate or patent attorney. Our particular firm used a closing software named Qualia and after close contact and numerous requests for new features with their team, I found myself gravitating more towards the technological side of our process.
+						</Text>
+						<Text>
+							Subsequently, I came to a relaization that I was in the wrong occupational field and made the decision to pursue my true aspiration which is front-end and back-end development!
+						</Text>
+					</GridItem>
+					<GridItem pl="2" area={'image'}>
+						Image
+					</GridItem>
+				</Grid>
+			</Container>
+			<Container as='div' p="100px 0px">
+				<Heading as="h2" pb="10px" size="xl">
+					Hobbies
+				</Heading>
+				<Text pb="10px">
+					As a professional web developer, I possess a passion for crafting
+					innovative and intuitive web experiences that effectively contribute to
+					the overall improvement of patient health outcomes while simultaneously
+					driving future funding success. I am committed to delivering exceptional
+					quality and ensuring that the end product not only meets but surpasses
+					the expectations of all stakeholders involved.
+				</Text>
+			</Container>
+		</Container>
+	);
+};
 
-export default Home
+export default Home;
